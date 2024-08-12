01daa18ed416b5dc917280115b6855f428c3c5b5
Tristan Fry <tFry@talosenergyllc.com>
2024-08-12 12:50:41 -0500

    test'

--- web-api/obj/Debug/net8.0/web-api.AssemblyInfo.cs
+++ web-api/obj/Debug/net8.0/web-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c3c45da5ac416678dabc97be108d6dda1d7067")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b42cc4dda72b2fbcc19e9a1c330f1645c939244")]
 [assembly: System.Reflection.AssemblyProductAttribute("web-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
